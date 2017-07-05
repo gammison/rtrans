e5ea1a39038948f1b607ab3c944edff17cff4963
Garrison Grogan <garrison.grogan@columbia.edu>
2017-07-05 14:43:22 -0400

    added tess2 class

--- tess2.h
+++ tess2.h
@@ -44,6 +44,24 @@ struct vector2D{
         *this = *this * s;
         return *this;
     }
-
-
 }
+
+class Tess2D {
+    private:
+        int locNumParticles;
+        int totNumParticles;
+
+        void *ptess; //present tessellation
+        void *vertList;
+
+    public:
+        Tess2d(void);
+        void reset_tess(void);
+        int buildTesselation(double *x[3], double *radius, int numParticles, double huge);
+        int updateTesselation(double *x[3], int upNumParticles);
+        int count_number_of_faces(void);
+        int extrGeom(double* x[3], double* dcom[3], double* volume,
+                double* face_area, double* face_com[3], double* face_n[3], int* pair_i, int* pair_j,
+                std::vector< std::vector<int> > &neighbors);
+};
+#endif
