43259c8329a4b521f6a1c6c68cd1ddd22c22e9f9
Garrison Grogan <garrison.grogan@columbia.edu>
2017-07-20 14:35:26 -0400

    started cellcontainer, will go back and do cellArray now

---
+++ utilities/cellContainer.pyx
@@ -0,0 +1,8 @@
+import numpy as np
+cimport numpy as np
+
+from cpython cimport pyDict_Contains, PyDict_GetItem
+
+from cellArray import baseArray, doubleArray, intArray, longArray, longLongArray
+
+cdef 
