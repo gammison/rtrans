054c03a6c7623a8680154d74f8ce3960e5f00d14
Garrison Grogan <garrison.grogan@columbia.edu>
2017-07-31 17:15:22 -0400

    implementing functions

--- utilities/cellArray.pyx
+++ utilities/cellArray.pyx
@@ -24,4 +24,31 @@ cdef class baseArray:
     raise NotImplementedError("baseArray::getCtype")
   cpdeef reserve(self, int size):
     #resizes data to required size.
-    raise NotImplementedError("baseArray:reserve")
+    raise NotImplementedError("baseArray::reserve")
+  cpdef resize(self, int size):
+    #resizes internal data to required size
+    raise NotImplementedError("baseArray::resize")
+  cpdef np.ndarray getNumPyArray(self):
+    #returns the numpy array of the data, deos not keeps its reference
+    return self._npy_array
+  cpdef squeeze(self):
+    #release unused memory
+    raise NotImplementedError("baseArray::squeeze")
+
+  cpdef remove(self, np.ndarray indexList, bint sortedInput=0):
+    #remove particles with given indices in indexList
+    raise NotImplementedError("baseArray::remove")
+  cpdef extend(self, np.ndarray inputArray):
+    #Extend array with data from inputArray
+    raise NotImplementedError("baseArray::extend")
+  cpdef alignArray(self, np.ndarray newIndices):
+    #rearrange array contents according to new indices
+    raise NotImplementedError("baseArray::alignArray")
+  cpdef reset(self):
+    #reset the lenght of the array to 0
+    cdef PyArrayObject* arr = <PyArrayObject*> self._npy_array
+    self.length = 0
+    arr.dimensions[0] = self.length
+  cpdef shrink(self, int size):
+    #reset length of array to give size
+    cdef PyArrayObject* arr = <PyArrayObject*> self._npy_array
